44ff8c625ceb577b585d3dd827c936b3085c5fb2
suryadatta1 <suryadattatangirala@outlook.com>
2019-12-30 20:32:58 +0530

    Updates

---
+++ precache-manifest.252027925b54c0c94a9c11dfd760537f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67512ed603794781015f277b9411bdb7",
+    "url": "/monoster-rolodex/index.html"
+  },
+  {
+    "revision": "e9252c597e83b6b0bc06",
+    "url": "/monoster-rolodex/static/css/main.cd530d8e.chunk.css"
+  },
+  {
+    "revision": "a467209c01c621b81a4b",
+    "url": "/monoster-rolodex/static/js/2.73f0c029.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monoster-rolodex/static/js/2.73f0c029.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e9252c597e83b6b0bc06",
+    "url": "/monoster-rolodex/static/js/main.4dca9d7b.chunk.js"
+  },
+  {
+    "revision": "73f25083834f07d747d4",
+    "url": "/monoster-rolodex/static/js/runtime-main.febf20d5.js"
+  }
+]);
